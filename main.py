8caf963008f5424cfe5b11c6562f3d4e4d68c1ad
gabsesq <gabbyesquibel@hotmail.com>
2024-12-12 16:32:30 -0700

    retail and WH sheets

--- main.py
+++ main.py
@@ -137,5 +137,5 @@ def get_lot_codes():
         return jsonify({"status": "error", "message": str(e)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
 
